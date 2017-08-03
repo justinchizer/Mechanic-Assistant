393ce0feba951276842351b0530bffaadea2b0c5
Justin Chizer <justin.chizer@gmail.com>
2017-08-03 12:34:32 -0700

    Skype bot should work now

--- hello.js
+++ hello.js
@@ -1,19 +1,21 @@
 var builder = require('botbuilder');
 var restify = require('restify');
-var dotenv = require('dotenv');
-
-var connector = new builder.ChatConnector({
-    appID: process.env.MICROSOFT_APP_IP,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
-var bot = new builder.UniversalBot(connector); // creates bot
+var dotenv = require('dotenv').config(); //I DONT KNOW IF THIS IS RIGHT
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
+
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+var bot = new builder.UniversalBot(connector); // creates bot
 server.post('/api/messages', connector.listen());
+
 
 /*bot.dialog('/', function(session) {
     //session.send('Hello, bot!') // dialog
