770973d1bd9ba2ffad77e09dfb142f5b0324e49d
Justin Chizer <justin.chizer@gmail.com>
2017-08-07 18:27:30 -0700

    Square 1

--- hello.js
+++ hello.js
@@ -1,65 +1,324 @@
-var builder = require('botbuilder');
-var restify = require('restify');
-var dotenv = require('dotenv').config(); //I DONT KNOW IF THIS IS RIGHT
+'use strict';
 
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+
+
+// INIT NODE MODULES
+
+var botbuilder = require('botbuilder');
+var dotenv = require('dotenv').config();
+
+
+// Setup Restify Server
+
+var botServer = require('restify').createServer();
+
+
+
+botServer.listen(process.env.port || process.env.PORT || 3978, function () {
+
+    console.log('%s listening to %s', botServer.name, botServer.url);
+
 });
 
 
-var connector = new builder.ChatConnector({
+
+// lets us know our bot is online when visiting the deployment URL in browser
+
+botServer.get('/', function (request, response, next) {
+
+    response.send({ status: 'online' });
+
+    next();
+
+});
+
+
+
+// status route
+
+botServer.get('/status', function statusHandler(request, response, next) {
+
+    response.send('Bot server online.');
+
+    next();
+
+});
+
+
+
+// Create chatConnector for communicating with the Bot Framework Service
+
+var chatConnector = new botbuilder.ChatConnector({
+
     appId: process.env.MICROSOFT_APP_ID,
+
     appPassword: process.env.MICROSOFT_APP_PASSWORD
+
 });
 
-var bot = new builder.UniversalBot(connector); // creates bot
-server.post('/api/messages', connector.listen());
 
 
-/*bot.dialog('/', function(session) {
-    //session.send('Hello, bot!') // dialog
-    var userMessage = session.message.text;
-    session.send('you said: ' + userMessage);
+// Listen for messages from users
 
-});*/
+botServer.post('/api/messages', chatConnector.listen());
 
-/*bot.dialog('/', [
-    function(session) {
-        builder.Prompts.text(session, 'Hello, what is your name?');
-    },
-    function(session, result) {
-        session.send('Hi ' + result.response + '! What can I do for you today?');
-    }
-]);*/
+
+
+// Create your bot with a function to receive messages from the user
+
+var bot = new botbuilder.UniversalBot(chatConnector);
+
+
+
+var HeroCardName = 'Hero Card';
+
+var SlackButtonsName = 'Slack Buttons';
+
+var BasicTextName = 'Basic Text';
+
+var CodeFormatTextName = 'Code Formatting';
+
+var SlackChannelData = 'Slack Channel Data';
+
+var DemoNames = [
+
+    HeroCardName,
+
+    SlackButtonsName,
+
+    BasicTextName,
+
+    CodeFormatTextName,
+
+    SlackChannelData
+
+];
+
+
 
 bot.dialog('/', [
-    function (session){
-        session.beginDialog('/phonePrompt');
+
+    function (session) {
+
+        session.send('Hi, I am Slack Support Bot!\n\n I will show you what is possible with Bot Framework for Slack.');
+
+
+
+        botbuilder.Prompts.choice(session, 'Choose a demo', DemoNames);
+
     },
-    function (session, results) {
-        session.send('Got it, your number is: %s', results.response);
+
+    function (session, result) {
+
+        console.log('result:\n');
+
+        console.log(result);
+
+
+
+        switch (result.response.entity) {
+
+            case HeroCardName:
+
+                session.beginDialog('herocard');
+
+                break;
+
+            case SlackButtonsName:
+
+                session.beginDialog('buttons');
+
+                break;
+
+            case BasicTextName:
+
+                session.beginDialog('basicMessage');
+
+                break;
+
+            case CodeFormatTextName:
+
+                session.beginDialog('code-formatting');
+
+                break;
+
+            case SlackChannelData:
+
+                session.beginDialog('slack-channel-data');
+
+                break;
+
+            default:
+
+                session.send('invalid choice');
+
+                break;
+
+        }
+
     }
+
 ]);
 
-bot.dialog('/phonePrompt', [
-    function(session, args) {
-        if (args && args.reprompt) {
-            builder.Prompts.text(session, "What is your phone number?");
-        } else {
-            builder.Prompts.text(session, "Please enter in xxx-xxx-xxxx format")
-        }
-    },
-    function (session, results) {
-        var matched = results.response.match(/\d+/g);
-        var number = matched ? matched.join('') : '';
-        if (number.length == 10) {
-            session.endDialogWithResult({response: number });
-        } else {
-            session.replaceDialog('/phonePrompt', {reprompt: true});
-        }
+
+
+bot.dialog('herocard', [
+
+    function (session) {
+
+        var heroCard = new botbuilder.Message(session)
+
+            .textFormat(botbuilder.TextFormat.xml)
+
+            .attachments([
+
+                new botbuilder.HeroCard(session)
+
+                    .title("Hero Card")
+
+                    .subtitle("Space Needle")
+
+                    .text("The Space Needle is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+
+                    .images([
+
+                        botbuilder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+
+                    ])
+
+                    .tap(botbuilder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+
+            ]);
+
+        session.endDialog(heroCard);
+
     }
+
 ]);
 
 
+
+bot.dialog('basicMessage', function (session) {
+
+    session.endDialog('A basic message');
+
+});
+
+
+
+bot.dialog('exit', function (session) {
+
+    session.endConversation('Goodbye!');
+
+}).triggerAction({ matches: /exit/i });
+
+
+
+bot.dialog('buttons', [
+
+    function (session) {
+
+        session.send('What kind of sandwich would you like?');
+
+
+
+        botbuilder.Prompts.choice(session, "Choose a sandwich:", ["Tuna", "Roast Beef", "Veggie Special"]);
+
+    },
+
+    function (session, result) {
+
+        session.endDialog(`You picked: ${result.response.entity}`)
+
+    }
+
+]).triggerAction({ matches: /button/i });
+
+
+
+bot.dialog('code-formatting', function (session) {
+
+    session.send("Code with back-tick style code formatting: `var foo = 'bar';`");
+
+    session.send("Code in double parens with single backtick `var code = 'formatted correctly?';`");
+
+    session.send("Code in double parens with triple backtick: ```var code.formatted = true;```");
+
+    session.send('Code in single parens with single backtick `var code = "formatted correctly?"`');
+
+    session.send('Code in single parens with triple backtick: ```var code.formatted = true;```');
+
+    session.endDialog();
+
+}).triggerAction({ matches: /code/i });
+
+
+
+bot.dialog('slack-channel-data', function(session) {
+
+    var message = {};
+
+    message.channelData = {
+
+        text: ":tada: Code with back-tick style code formatting: `var foo = 'bar';`"
+
+    }
+
+    session.send("Sending first message:");
+
+    session.send(message);
+
+
+
+    session.send("Sending second message:");
+
+    session.send({"channelData": { "text": "Code in double parens with triple backtick: ```var code.formatted = true;```"}});
+
+
+
+    session.send("Sending third message:");
+
+    session.send({"channelData": { 'text': 'Code in single parens with single backtick `var code = "formatted correctly?"`'}});
+
+
+
+    session.send("Sending fourth message:");
+
+    session.send({"channelData": { 'text': 'Code in single parens with triple backtick: ```var code.formatted = true;```'}});
+
+
+
+    session.send("Sending fifth message:");
+
+    session.send({ 
+
+        channelData: {
+
+            text: "channel data text with code formatting: \`var foo = 'formatted text'\`"
+
+        }
+
+    });
+
+
+
+    session.send("Sending 6th message:");
+
+    session.send({ 
+
+        "channelData": {
+
+            "text": "*Channel data* _text_ with triple backtick:\n```var foo = 'formatted text'```"
+
+        }
+
+    });
+
+
+
+    session.endDialog();
+
+
+
+}).triggerAction({matches: /channel/i});
 
